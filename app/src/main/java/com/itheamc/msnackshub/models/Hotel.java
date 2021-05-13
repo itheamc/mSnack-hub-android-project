cc8e9c72bb094cacb23c65ef9a83397e0a1d3fed
Amit Chaudhary <itheamc@gmail.com>
2021-05-13 18:18:35 +0545

    Constructor added in Hotel Model

--- app/src/main/java/com/itheamc/msnackshub/models/Hotel.java
+++ app/src/main/java/com/itheamc/msnackshub/models/Hotel.java
@@ -16,6 +16,22 @@ public class Hotel {
     private Boolean _is_active;
     private String _api_key;
 
+
+    // Constructor
+    public Hotel(String _id, String _name, String _address, String _owner, String _image, Double _latitude, Double _longitude, String _listed_on, String _established_on, Boolean _is_active, String _api_key) {
+        this._id = _id;
+        this._name = _name;
+        this._address = _address;
+        this._owner = _owner;
+        this._image = _image;
+        this._latitude = _latitude;
+        this._longitude = _longitude;
+        this._listed_on = _listed_on;
+        this._established_on = _established_on;
+        this._is_active = _is_active;
+        this._api_key = _api_key;
+    }
+
     // Creating DiffUtil.ItemCallback
     public static DiffUtil.ItemCallback<Hotel> hotelItemCallback = new DiffUtil.ItemCallback<Hotel>() {
         @Override
